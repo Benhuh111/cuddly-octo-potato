ac67a3e345fe74e18c1ce451fc7d74a7c50ebb04
Benny <bennyhuang1996@outlook.com>
2025-08-15 03:49:51 +0200

    Initial code

---
+++ src/main/java/com/example/cuddly_octo_potato/CuddlyOctoPotatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuddly_octo_potato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuddlyOctoPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuddlyOctoPotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cuddly_octo_potato/CuddlyOctoPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuddly_octo_potato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuddlyOctoPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
